c34c6937e375482981d76988e30fbf693479f792
Sergi Metcalf Quintana <sergi.metcalf@estudiantat.upc.edu>
2024-10-09 21:20:55 +0200

    Añadir nueva classe SpecialTile

---
+++ 2DVideogame/2DVideogame/SpecialTile.cpp
@@ -0,0 +1,14 @@
+#include "SpecialTile.h"
+
+
+
+SpecialTile::SpecialTile()
+{ 
+	  
+
+}   
+
+
+SpecialTile::~SpecialTile()
+{
+}
---
+++ 2DVideogame/2DVideogame/SpecialTile.h
@@ -0,0 +1,10 @@
+#pragma once
+class SpecialTile
+{
+public:
+	SpecialTile();
+	~SpecialTile();
+
+
+};
+
